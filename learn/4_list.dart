dc5c86685cac48003a33f81c8f23ae5d1b816fc1
NguyenLong <nguyenlong160104@gmail.com>
2025-06-04 23:09:49 +0700

    Dart

--- learn/4_list.dart
+++ learn/4_list.dart
@@ -1,15 +1,35 @@
 import 'dart:io';
 // LIST
 void main() {
-  var objects = <String>[]; // Tạo list rỗng có kiểu dữ liệu cụ thể
+  var objects = []; // Tạo list rỗng có kiểu dữ liệu cụ thể
+  var doubles = <double>[]; // Tạo list rỗng có kiểu dữ liệu cụ thể double
+
+  objects.add(1); // Thêm phần tử vào list
+  objects.add('Hello'); // Thêm phần tử vào list
+  objects.add(true); // Thêm phần tử vào list
+  print(objects); // In ra list
+
+  doubles.add(1.9); // Thêm phần tử vào list
+  print(doubles); // In ra list
+
   var numbers = [1,2,3,4,5];
-  var friends = ['Long','Hung'];
-  // Them vao listnumbers.add(100);
-  //   friends.add('Linh');
+  var friends = ['Long','Hung','Khanh','Mai','Minh'];
+
+  numbers.add(90);
+  friends.add('Linh');
 
   // Lay du lieu
-  print('number[0]: ${numbers[0]}');
-  print('number[length - 1]: ${numbers[numbers.length - 1]}');
+  print('So phan tu trong list: ${numbers.length}'); // Lay so phan tu trong list
+  print('number[0]: ${numbers[0]}');  // Lay phan tu dau tien trong list
+  print('number[length - 1]: ${numbers[numbers.length - 1]}');  // Lay phan tu cuoi cung trong list
 
+  friends.first;  // Lay phan tu dau tien trong list
+  friends.last;   // Lay phan tu cuoi cung trong list
+  friends.reversed; // Dao nguoc list
+
+  friends.removeAt(0); // Xoa phan tu dau tien trong list
+  friends.removeLast(); // Xoa phan tu cuoi cung trong list
+  friends.remove('Hung'); // Xoa phan tu 'Hung' trong list
+  friends.removeRange(0, 2); // Xoa phan tu tu vi tri 0 den 2 trong list
 
 }
--- learn/5_for_loop.dart
+++ learn/5_for_loop.dart
@@ -1,13 +1,14 @@
 // FOR LOOP
 void main() {
   var numbers = [1,2,3,4,5];
-  var friends = ['Long','Hung'];
+  var friends = ['Long','Hung','Khanh','Mai','Minh'];
 
   numbers.add(100);
   numbers.insert(2, 200);
   friends.add('Linh');
 
+  print('==> Danh sach chi so va gia tri: ');
   for (var index = 0; index < numbers.length; index ++) {
-    print(numbers[index]);
+    print('$index: ${numbers[index]}');
   }
 }
