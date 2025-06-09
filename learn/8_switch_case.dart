3239d9421ad2b1152399e312de1a6cbec967d58d
NguyenLong <nguyenlong160104@gmail.com>
2025-06-09 16:28:25 +0700

    Switch statement

--- learn/8_switch_case.dart
+++ learn/8_switch_case.dart
@@ -1,1 +1,34 @@
-// Switch
+// Switch statement
+import 'dart:io';
+
+void main() {
+  print("number a: ");
+  var a = int.parse(stdin.readLineSync()!);
+  print("number b: ");
+  var b = int.parse(stdin.readLineSync()!);
+
+  print("input : ");
+  var opera = stdin.readLineSync()!;
+
+  Object result;
+  switch (opera) {
+    case '+':
+      result = a + b;
+      print(result);
+      break;
+    case '-':
+      result = a - b;
+      print(result);
+      break;
+    case '*':
+      result = a * b;
+      print(result);
+      break;
+    case '/':
+      result = (a / b);
+      print(result);
+      break;
+    default:
+      throw FormatException("Invalid operator");
+  }
+}
