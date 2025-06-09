843b438e04fde8f7a5eb1945a217680d0087dae8
NguyenLong <nguyenlong160104@gmail.com>
2025-06-09 16:06:57 +0700

    final & const

--- learn/7_final_const.dart
+++ learn/7_final_const.dart
@@ -1,5 +1,17 @@
 // Final & Const : Hằng số
+import 'dart:io';
+
 void main() {
-  final x = 100;
-  
+  // Hằng số x
+  final X = 100;
+  final String MY_NAME = "Long";
+  final double PI = 3.1415;
+
+  const int Y = 200;
+  print("Bán kinh đường tròn ?");
+  var r = double.parse(stdin.readLineSync()!);
+
+  var area = PI * r * r;
+
+  print("Dien tich hinh tron la: $area");
 }
