8fbc8ed9623fd523cdba4b27dba48068c0d59fad
NguyenLong <nguyenlong160104@gmail.com>
2025-07-02 10:46:40 +0700

    do while

--- learn/9_while.dart
+++ learn/9_while.dart
@@ -1,1 +1,34 @@
 // While, Do while
+import 'dart:math';
+
+void main() {
+  // int n = 100;
+  // int sum = 0;
+  //
+  // do {
+  //   sum += n;
+  //   n--;
+  // } while (n >= 1);
+  // print('Tong cac so tu 1 den $n la: $sum');
+
+  int n = 5;
+  bool isPrime = true;
+  int bound = sqrt(n).toInt();
+  int i = 2;
+
+  if (n < 2) {
+    isPrime = false;
+  }
+
+  while (i <= bound) {
+    if (n % i == 0) {
+      isPrime = false;
+    }
+    i++;
+  }
+  if (isPrime) {
+    print('$n la so nguyen to');
+  } else {
+    print('$n khong phai la so nguyen to');
+  }
+}
