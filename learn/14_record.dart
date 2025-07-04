0fdb31afdc84e1cb0fc94902486eca88bd760be6
NguyenLong <nguyenlong160104@gmail.com>
2025-07-04 10:36:25 +0700

    record

--- learn/14_record.dart
+++ learn/14_record.dart
@@ -1,18 +1,20 @@
+// Kiểu record
 void main() {
-  var day = dayOfWeek(3);
-  print('day = ${day.$1} - ${day.$2}');
+  // var record = ('first',age : 200,gpa: 3.65, 'last');
+  // print('${record.$1}');
+  // print('${record.age}');
+  // print('${record.$2}');
+  // print('${record.gpa}');
+
+  (String, int) record;
+  record = ('first', 200);
+  print('${record.$1}');
+  print('${record.$2}');
+
+  var result = stringToNumber();
+  print(result.$1);
 }
 
-void printElement(int x)=> print(x);
-
-double avg(int a, int b, int c)=> (a + b + c) / 3;
-
-// Kiểu record
-(String day, String dayInVn) dayOfWeek(int day) {
-  return switch (day){
-    1 => ('Monday', 'Thứ hai'),
-    2 => ('Tuesday', 'Thứ ba'),
-    3 => ('Wednesday', 'Thứ tư'),
-    _=> ('Unknown', 'Không rõ')
-  };
+(String , int) stringToNumber() {
+  return ("one", 1) ;
 }
